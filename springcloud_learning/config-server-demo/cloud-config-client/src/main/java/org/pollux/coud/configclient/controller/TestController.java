745d78a64acc891babfeb261731db8766c517028
cnodin <cnodin@gmail.com>
2016-10-04 00:31:59 +0800

    commit cloud-config-client

--- springcloud_learning/config-server-demo/cloud-config-client/src/main/java/org/pollux/coud/configclient/controller/TestController.java
+++ springcloud_learning/config-server-demo/cloud-config-client/src/main/java/org/pollux/coud/configclient/controller/TestController.java
@@ -1,7 +1,26 @@
 package org.pollux.coud.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by pollux on 16/10/3.
  */
+@RefreshScope
+@RestController
 public class TestController {
+
+  @Value("${from}")
+  private String from;
+
+
+  @RequestMapping("/from")
+  public String from(){
+
+    return this.from;
+  }
+
 }
