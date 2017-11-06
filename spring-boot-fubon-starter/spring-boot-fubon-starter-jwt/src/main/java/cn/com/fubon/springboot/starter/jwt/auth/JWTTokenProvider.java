e7f5f6d5a246ecafa191be32f00698e936cf5a76
cnodin <cnodin@gmail.com>
2017-11-06 08:49:38 +0800

    modify createToken

--- spring-boot-fubon-starter/spring-boot-fubon-starter-jwt/src/main/java/cn/com/fubon/springboot/starter/jwt/auth/JWTTokenProvider.java
+++ spring-boot-fubon-starter/spring-boot-fubon-starter-jwt/src/main/java/cn/com/fubon/springboot/starter/jwt/auth/JWTTokenProvider.java
@@ -7,12 +7,15 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mobile.device.Device;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.*;
@@ -31,6 +34,9 @@ import java.util.stream.Collectors;
 @Service
 public class JWTTokenProvider implements TokenProvider {
 
+    @Value("application.name")
+    private String applicationName;
+
     private JwtSecurityProperties jwtProperties;
 
     private long tokenValidityInSecondsForRemberMe;
@@ -40,6 +46,11 @@ public class JWTTokenProvider implements TokenProvider {
     public JWTTokenProvider(JwtSecurityProperties jwtProperties){
         this.jwtProperties = jwtProperties;
     }
+
+    static final String AUDIENCE_UNKNOWN = "unknown";
+    static final String AUDIENCE_WEB = "web";
+    static final String AUDIENCE_MOBILE = "mobile";
+    static final String AUDIENCE_TABLET = "tablet";
 
     @PostConstruct
     public void init(){
@@ -56,15 +67,27 @@ public class JWTTokenProvider implements TokenProvider {
         String authorities = extractAuthorities(auth.map(Authentication::getAuthorities)
                             .orElseThrow(IllegalArgumentException::new));
 
+        Date now = new Date(System.currentTimeMillis());
         Date validity = provideValidityDate(rememberMe);
 
         String subject = auth.map(Authentication::getName).orElseThrow(IllegalArgumentException::new);
 
-        JwtBuilder jwtBuilder = Jwts.builder().setSubject(subject)
-                            .signWith(SignatureAlgorithm.HS256, jwtProperties.getToken().getSecret())
-                            .setExpiration(validity);
+//        String audience = generateAudience(device);
 
-        if (!"".equals(authorities.trim())){
+        JwtBuilder jwtBuilder = Jwts.builder()
+                            .setHeaderParam("typ", "JWT")
+                            .setIssuedAt(now)           //创建时间
+                            .setIssuer(applicationName) //签发者
+                            .setSubject(subject)        //主题，一般是个人信息
+                            //.setAudience(generateAudience)
+                            .setExpiration(validity)    //过期时间
+                            .setNotBefore(now)          //系统时间之前的token都是不被承认的
+                            .signWith(SignatureAlgorithm.HS256,
+                                    jwtProperties.getToken().getSecret())
+                            ;
+
+        if (!StringUtils.isEmpty(authorities.trim())){
+            //这里的claim是public claim，可以添加自己的字段
             jwtBuilder.claim(jwtProperties.getToken().getPayload().getAuthoritiesKey(), authorities);
         }
         return jwtBuilder.compact();
@@ -109,5 +132,18 @@ public class JWTTokenProvider implements TokenProvider {
         long now = System.currentTimeMillis();
         return rememberMe ? new Date(now + this.tokenValidityInSecondsForRemberMe)
                         : new Date(now + this.tokenValidityInSeconds);
+    }
+
+    private String generateAudience(Device device){
+        String audience = AUDIENCE_UNKNOWN;
+        if (device.isNormal()) {
+            audience = AUDIENCE_WEB;
+        } else if (device.isTablet()) {
+            audience = AUDIENCE_TABLET;
+        } else if (device.isMobile()) {
+            audience = AUDIENCE_MOBILE;
+        }
+
+        return audience;
     }
 }
